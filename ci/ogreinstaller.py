424ccaa2929d8a627a090cc30645d5f2a81570e7
intelligide <intelligide@hotmail.fr>
2020-05-02 16:35:13 -0400

    Fix windows ci

--- ci/ogreinstaller.py
+++ ci/ogreinstaller.py
@@ -1,20 +1,8 @@
-import requests
-from tempfile import NamedTemporaryFile
+import os
+from io import BytesIO
+from urllib.request import urlopen
 from zipfile import ZipFile
-
-def fetch(url: str, path: str):
-    with requests.get(url, stream=True) as stream, NamedTemporaryFile() as file:
-
-        # Download
-        stream.raise_for_status()
-        for chunk in stream.iter_content(chunk_size=8192):
-            file.write(chunk)
-        file.flush()
-
-        # extract zip file
-        with ZipFile(path, "r") as zip:
-            zip.extractall(path)
-
-windows_url = "https://bintray.com/ogrecave/ogre/download_file?file_path=ogre-sdk-v1.12.6-vc15-x64.zip"
-
-fetch(windows_url, "libs/Ogre")
+zipurl = "https://bintray.com/ogrecave/ogre/download_file?file_path=ogre-sdk-v1.12.6-vc15-x64.zip"
+with urlopen(zipurl) as zipresp:
+    with ZipFile(BytesIO(zipresp.read())) as zfile:
+        zfile.extractall(os.path.join(os.getcwd(), "libs", "Ogre"))
